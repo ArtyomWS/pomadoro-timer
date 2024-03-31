0af2976049e16824c5834851219be9d8caddff30
ArtyomgdcGroup <artyom.serdyuk@gdcgroup.com>
2024-03-31 14:58:19 +0300

    add: main window ui

---
+++ main.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+tomato_img = PhotoImage(file="tomato.png")
+canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
+canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+canvas.pack()
+
+
+window.mainloop()
